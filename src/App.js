b47458b0cd1e8f901141aba6113e0aaa3d60f40c
Jay <jay@Jays-MacBook-Pro.local>
2022-12-07 01:16:34 +0530

    feat: Added mui custom theme for color palette

--- src/App.js
+++ src/App.js
@@ -6,18 +6,31 @@ import { Route, Routes } from 'react-router-dom';
 import PageNotFound from './components/PageNotFound';
 import Chatbox from './components/Chatbox';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0097a7',
+    },
+    secondary: {
+      main: '#00838f',
+    },
+  },
+});
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Login />} exact />
-        <Route index element={<Login />} />
-        <Route path='chatbox' element={<Chatbox />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route index element={<Login />} />
+          <Route path='chatbox' element={<Chatbox />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
