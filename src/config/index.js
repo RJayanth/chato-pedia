cf506882b803dba468347defbd5c89f6118fb48f
Jayanth R <r.jayanth@outlook.com>
2022-12-11 22:37:49 +0530

    chore: change config as per deployment changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 const BASE_URL = window.location.href;
-const APP_ENV = BASE_URL.includes('localhost')
+const APP_ENV = BASE_URL.includes('localhost') || BASE_URL.includes('192.168.0.105')
   ? {
-      host: 'localhost',
+      host: '192.168.0.105',
       port: '4000',
     }
   : {
       host: 'chatopedia.com',
-      port: '80',
+      port: '8080',
     };
 export { APP_ENV };
